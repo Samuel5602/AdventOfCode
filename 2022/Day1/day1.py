efb92612a0dd565aa3c78545fd5d6ccc0250b253
Samuel Dankers <samuel.dankers@lumenbs.nl>
2022-12-02 11:02:36 +0100

    completed day 1 challenge 2

--- 2022/Day1/day1.py
+++ 2022/Day1/day1.py
@@ -1,3 +1,4 @@
+# Challenge 1
 with open('./2022/Day1/day1_input.txt', 'r') as f:
     lines = f.read()
 
@@ -10,3 +11,7 @@ for i in range(len(lines)):
 
 max = max([sum(f) for f in lines])
 print(max)
+
+# Challenge 2
+sum_list = sorted([sum(f) for f in lines], key=lambda t: -t)
+print(sum(sum_list[:3]))
