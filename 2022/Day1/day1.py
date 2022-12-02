b8151b296803ac170fdc5a6f87aae426f92d084a
Samuel Dankers <samuel.dankers@lumenbs.nl>
2022-12-02 12:17:36 +0100

    completed day 2 challenge 2

--- 2022/Day1/day1.py
+++ 2022/Day1/day1.py
@@ -2,9 +2,7 @@
 with open('./2022/Day1/day1_input.txt', 'r') as f:
     lines = f.read()
 
-lines = lines.split('\n\n')
-
-lines = [f.split() for f in lines]
+lines = [f.split() for f in lines.split('\n\n')]
 
 for i in range(len(lines)):
     lines[i] = [int(f) for f in lines[i]]
@@ -13,5 +11,5 @@ max = max([sum(f) for f in lines])
 print(max)
 
 # Challenge 2
-sum_list = sorted([sum(f) for f in lines], key=lambda t: -t)
+sum_list = sorted([sum(f) for f in lines], reverse=True)
 print(sum(sum_list[:3]))
--- 2022/Day2/day2.py
+++ 2022/Day2/day2.py
@@ -1,12 +1,9 @@
-# a=x=rock, b=y=paper, c=z=scissors
 # Challenge 1
 with open('./2022/Day2/day2_input.txt', 'r') as f:
     lines = f.readlines()
 
-# print(len(lines))
 
 def get_score(f0, f1):
-    # print(f'{f0}: {ord(f0)};    {f1}: {ord(f1)}')   
     win = 0
     if ord(f0) == ord(f1)-23:
         win = 3
@@ -15,18 +12,21 @@ def get_score(f0, f1):
 
     return win + (ord(f1) - 87)
 
-
 scores = [get_score(f[0], f[2]) for f in lines]
 print(sum(scores))
 
-'''
-win:
-A(65) Y(89) -> diff 24
-B(66) Z(90) -> diff 24
-C(67) X(88) -> diff 21
+# Challenge 2
+def get_score2(f0, f1):
+    if f1 == 'X':
+        return (ord(f0)%3) + 1
+    elif f1 == 'Y':
+        return ord(f0) - 61
+    else:
+        if f0 == 'B':
+            return 6 + 3
+        else:
+            return 6 + (2 if f0 == 'A' else 1)
 
-Lose:
-A(65) Z(90) -> diff 25
-B(66) X(88) -> diff 22
-C(67) Y(89) -> diff 22
-'''
+scores = [get_score2(f[0], f[2]) for f in lines]
+print(sum(scores))
+
