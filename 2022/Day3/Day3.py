3af6731c1305bb426439f64bbdce1dcdcf905019
Samuel Dankers <samuel.dankers@lumenbs.nl>
2022-12-04 22:28:38 +0100

    last changes day 3 challenge 1

--- 2022/Day3/Day3.py
+++ 2022/Day3/Day3.py
@@ -3,7 +3,7 @@ with open('./2022/Day3/day3_input.txt', 'r') as f:
     lines = [g[:-1] for g in f.readlines()]
 
 # Challenge 1
-compartments = [(set(f[:int(len(f)/2)]).intersection(set(f[-int(len(f)/2):])), ord(str(set(f[:int(len(f)/2)]).intersection(set(f[-int(len(f)/2):])).pop()))) for f in lines]
+compartments = [set(f[:int(len(f)/2)]).intersection(set(f[-int(len(f)/2):])) for f in lines]
 
-prios = [ord(f[0].pop()) - (96 if list(f)[1] > 97 else 38) for f in compartments]
+prios = [ord(list(f)[0]) - (96 if ord(f.pop()) > 97 else 38) for f in compartments]
 print(sum(prios))
