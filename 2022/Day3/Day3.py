d47f56d4fa7086b6e2889d4c6c3aa25caa49486e
Samuel Dankers <samuel.dankers@lumenbs.nl>
2022-12-04 22:29:41 +0100

    day 3 challenge 1 2 lines

--- 2022/Day3/Day3.py
+++ 2022/Day3/Day3.py
@@ -4,6 +4,6 @@ with open('./2022/Day3/day3_input.txt', 'r') as f:
 
 # Challenge 1
 compartments = [set(f[:int(len(f)/2)]).intersection(set(f[-int(len(f)/2):])) for f in lines]
+print(sum([ord(list(f)[0]) - (96 if ord(f.pop()) > 97 else 38) for f in compartments]))
 
-prios = [ord(list(f)[0]) - (96 if ord(f.pop()) > 97 else 38) for f in compartments]
-print(sum(prios))
+# Challenge 2
